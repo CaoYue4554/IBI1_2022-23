de12a6d2853827ff17d42ba6f0a7a012f327875e
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-03-13 19:39:56 +0800

    First trial

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,23 @@
+#Rabbits=2, generation=1
+#while rabbits <100,
+# generation+=1
+# rabbits*=2
+# newborn rabbits=rabbits/2
+# print(newborn rabbits)
+# loop over
+# print the generation that rabbits exceed 100.
+
+
+
+Rabbits=2
+generation=1
+while Rabbits <100:
+  generation+=1
+  Rabbits*=2
+  newborn_rabbits=Rabbits/2
+  print(newborn_rabbits)
+
+next_generation=str(generation)
+
+print("The rabbits will exceed 100 in the %sth generation." %next_generation)
+
