0e01e7e586895412b5ee42135ea6394f2ac4c508
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-03-13 16:44:05 +0800

    change = into ==

--- variables.py
+++ variables.py
@@ -1,12 +1,12 @@
-a=-3.19
+a==-3.19
 #Edinburgh longitude
-b=-118.24
+b==-118.24
 #LA logitude
-c=116.39
+c==116.39
 #Haining longitude
-b-a=d
+b-a==d
 print(d)
 #LA and Edinburgh
-c-a=e
+c-a==e
 #Haining and Edinburgh
 d>e
