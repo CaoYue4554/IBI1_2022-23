bcca0ddacc795b270047bd7587df0977edfeac58
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-04-13 11:41:53 +0800

    Added the function to get code sequence.

--- Practical9/count_cds_genes.py
+++ Practical9/count_cds_genes.py
@@ -1,5 +1,18 @@
 stopcode = input("Input your stop code:")
 import re
+def stops(stops,seq): # define the function to get the number of code sequence.
+    pos = re.findall(stops,seq)
+    count = 0
+    posss = seq.find("ATG") + 3 # Find the earliest ATG. +3 to make sure ATG will not overlap with the stop code.
+    for poss in range(len(pos)):
+        sit = posss + 1 # Start searching after ATG, so the stopcode will not appear earlier than ATG.
+        if seq.find(stops,sit) != -1:
+            posss = seq.find(stops,sit)
+            count += 1 # count times
+        else:
+            break
+    return count
+
 xfile = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
 out = open("genespre.fa","w")
 for lines in xfile:
