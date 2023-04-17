5fa1c82278d4b2eaa63259470d0fe680e2a05e5b
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-04-17 18:24:54 +0800

    Task3

---
+++ Practical10/protein_coding_capability_calculator.py
@@ -0,0 +1,14 @@
+import re
+def codecap(DNA):
+    DNA_change = re.sub("ATG.+?TGA","",DNA,flags=re.IGNORECASE)
+    percentage = 1-len(DNA_change)/len(DNA)
+    if percentage > 0.5:
+        print(percentage,"protein-coding")
+    elif percentage <0.1:
+        print(percentage,"non-coding")
+    else:
+        print(percentage,"unclear")
+    return percentage
+
+DNA = "AAAAtGGGGTGA"
+codecap(DNA)
