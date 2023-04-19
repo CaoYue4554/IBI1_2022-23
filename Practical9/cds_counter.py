3243cd2514f8982dae8738fc98da62cca56ebd34
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-04-19 16:34:20 +0800

    change my method.

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,16 +1,8 @@
 import re
-seq = "TGATAAATGATAATGATGA"
-def stops(stops,seq):
-    pos = re.findall(stops,seq)
-    count = 0
-    posss = seq.find("ATG") + 3 # Find the earliest ATG. +3 to make sure ATG will not overlap with the stop code.
-    for poss in range(len(pos)):
-        sit = posss + 1 # Start searching after ATG, so the stopcode will not appear earlier than ATG.
-        if seq.find(stops,sit) != -1:
-            posss = seq.find(stops,sit)
-            count += 1 # count times
-        else:
-            break
-    return count
-codeseq = stops("TAA",seq) + stops("TGA",seq) +stops("TAG", seq)
+def stops(stopcode,seq):
+    posss = int(seq.find("ATG") + 3) # Find the earliest ATG. +3 to make sure ATG will not overlap with the stop code.
+    num = len(re.findall(stopcode, seq[posss:]))
+    return num
+seq = "TGAATGTGATGA"
+codeseq = stops("TGA", seq) + stops("TAA", seq) + stops("TAG", seq)
 print(codeseq)
