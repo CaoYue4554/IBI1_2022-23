99dbe29187b8b62344d55c3253cbfd44b8d3d582
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-04-19 16:36:07 +0800

    change into an easier method.

--- Practical9/get_TGA_stops.py
+++ Practical9/get_TGA_stops.py
@@ -1,23 +1,17 @@
 import re
 xfile = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
-out = open("TGA_genespre.fa","w")
-for lines in xfile:
-    if re.match("[ATGC]",lines): # make the gene sequences on one line
-        lines =re.sub("\n","",lines)
-        out.write(lines)
-    if re.match(">",lines): # make the gene name shorter, and put it in one line with the genes
-        lines = re.sub(r'(>)','\n\\1',lines)
-        lines = re.sub(r"(cdna.+?\n)",'',lines) # delete the useless information after the name.
-        out.write(lines)
-
 out = open("TGA_genes.fa","w")
-fil = open("TGA_genespre.fa")
-for lines in fil:
-    if re.search("TGA$",lines):
-        lines = re.sub('\s','\n',lines)
+xxflie =xfile.read()
+DNAlist = re.split("\n>",xxflie)
+DNAlist_2 = []
+for item in range(len(DNAlist)):
+    items = re.sub(r"dna.+?\n", '', DNAlist[item])  # delete the useless information after the name.
+    items = re.sub("\n","",items)  # delete the \n inside the sequence.
+    items = re.sub("c","\n",items)  # change lines between the name of the gene and the sequence.
+    items = re.sub("^","\n>",items)  # add \n> at the beginning.
+    DNAlist_2.append(items)
+DNAlist_2[0] = re.sub("^\n>","",DNAlist_2[0])  # delete the \n on the first row.
+
+for lines in DNAlist_2:
+    if re.search("TGA$",lines):  # Find TGA
         out.write(lines)
-out.close()
-fil = open("TGA_genes.fa")
-for lines in fil:
-    print(lines)
-fil.close()
