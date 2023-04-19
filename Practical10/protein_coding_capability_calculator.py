7b5d07b81f79119ee7e57d2335d2e33e2fd4543c
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-04-19 13:09:15 +0800

    Change percentage

--- Practical10/protein_coding_capability_calculator.py
+++ Practical10/protein_coding_capability_calculator.py
@@ -1,13 +1,13 @@
 import re
 def codecap(DNA):
-    DNA_change = re.sub("ATG.+?TGA","",DNA,flags=re.IGNORECASE) #DNA change stores the sequence out of the coding sequence.
-    percentage = 1-len(DNA_change)/len(DNA)
+    DNA_change = re.sub("ATG.+?TGA","",DNA,flags=re.IGNORECASE)
+    percentage = 1-(len(DNA_change)+6)/len(DNA)
     if percentage > 0.5:
-        print(percentage,"protein-coding")
+        print(percentage*100,"%","protein-coding")
     elif percentage <0.1:
-        print(percentage,"non-coding")
+        print(percentage*100,"%","non-coding")
     else:
-        print(percentage,"unclear")
+        print(percentage*100,"%","unclear")
     return percentage
 
 DNA = "AAAAtGGGGTGA"
