d54cf5ae2cc4bc8ee5aae3b26eea1339c4c29ef4
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-05-17 14:43:20 +0800

    Script finished

--- Practical14/go_obo.py
+++ Practical14/go_obo.py
@@ -2,23 +2,45 @@ from openpyxl import Workbook
 import re
 import xml.dom.minidom
 
-
 # input the .xml file
 DOMtree = xml.dom.minidom.parse("go_obo.xml")
 collection = DOMtree.documentElement
 goobos = collection.getElementsByTagName("term")
 
+
+# define recursion to find all is_a
+def recursion(id, idlist, islist, counter):  # counter is involved to make sure the counter will not be cleaned every time I recurse.
+    if id not in islist:
+        return counter
+    indices = [i for i in range(len(islist)) if islist[i] == id]  # some have more than one index.
+    for pos in indices:
+        counter = recursion(idlist[pos], idlist, islist, counter)  # when the original one is found, keep searching the childnodes of this.
+        counter += 1
+    return counter
+
+
 # create the .xlsx file
-file = Workbook()
-thisfile = file.active
-thisfile.append(["id", "name", "definition", "childnodes"])
+workbook = Workbook()
+sheet = workbook.active
+sheet.append(["id", "name", "definition", "childnodes"])
+is_a_ID = []
+is_a = []
+
+for goobo in goobos:
+    ID = goobo.getElementsByTagName("id")[0].childNodes[0].data  # get the id
+    childnode = goobo.getElementsByTagName("*")
+    for node in childnode:
+        if node.tagName == "is_a":
+            for i in range(len(node.childNodes)):
+                is_a_ID.append(ID)
+                is_a.append(node.childNodes[i].data)
 
 for goobo in goobos:
     defstr = goobo.getElementsByTagName("defstr")[0].childNodes[0].data
     if re.search("Autophagosome", defstr, flags=re.I) is not None:  # search for all terms including "autophagosome"
-        ID = goobo.getElementsByTagName("id")[0].childNodes[0].data  # get the id
+        IDauto = goobo.getElementsByTagName("id")[0].childNodes[0].data  # get the id
         name = goobo.getElementsByTagName("name")[0].childNodes[0].data
-        childnodes = len(goobo.getElementsByTagName("is_a"))
-        thisfile.append([ID, name, defstr, childnodes])  # write into the .xlsx file
+        childnodes = recursion(IDauto, is_a_ID, is_a, 0)
+        sheet.append([IDauto, name, defstr, childnodes])  # write into the .xlsx file'''
 
-file.save('go_obo.xlsx')
+workbook.save(filename='go_obo.xlsx')
