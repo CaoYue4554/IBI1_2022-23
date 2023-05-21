8cc9c9b369e93c774acc32a396403f28e099b733
CaoYue4554 <yue.22@intl.zju.edu.cn>
2023-05-21 12:59:41 +0800

    code modified into less printings

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -2,47 +2,35 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-pd.set_option('display.max_columns',None)
+
+pd.set_option('display.max_columns', None)
 # show all columns
 os.chdir("D:/2023Grade1/IBI1")
 os.getcwd()
 os.listdir()
-covid_data=pd.read_csv("full_data.csv")
-print(covid_data.head(5))
-# Show the first 5 rows
-print(covid_data.info)
-print(covid_data.describe())
-# new case mean:1.117552e+04  SD:1.052843e+05
-# total cases:1.000000e+00-7.610711e+08
-print(covid_data.iloc[0:100:2,0:5])
-# iloc[x:y:z],means show the column from every 2 rows in the first 100 rows.
-print(covid_data.iloc[0:1001:100,1])
-my_columns=[True,False,True,False,True,False]
-print(covid_data.iloc[0:3,my_columns])
-# If my_columns is shorter or longer, it will report Index error.
-print(covid_data.loc[2:4,"date"])
-my_rows = covid_data["location"] == "Afghanistan"
-print(my_rows)
-# my_rows is
-# 0        True
-# 1        True
-# 2        True
-# 3        True
-# 4        True
-#         ...
-# 7991    False
-# 7992    False
-# 7993    False
-# 7994    False
-# 7995    False
+covid_data = pd.read_csv("full_data.csv")
+# the second column from every 100th row from the first 1000 rows (inclusive)
+print(covid_data.iloc[0:1001:100, 1])
+
+# "total cases" for all rows corresponding to Afghanistan.
+my_rows = []
+for i in range(len(covid_data)):
+    if covid_data.loc[i, "location"] == "Afghanistan":
+        my_rows.append(True)
+    else:
+        my_rows.append(False)
 print(covid_data.loc[my_rows, "total_cases"])
-new_data1=covid_data.loc[covid_data["location"]!="World",["date","location","new_cases","new_deaths"]]
-new_data=new_data1.loc[new_data1["date"]=="2020-03-31",["location","new_cases","new_deaths"]]
-print(new_data)
-print(new_data.describe())
-print(np.mean(new_data["new_cases"]))
-print(np.mean(new_data["new_deaths"]))
-# The mean of new_cases is 321.89, new_deaths is 19.06. the proportion is 0.05.
+
+# the mean number of new cases and new deaths on 31 March 2020.
+new_data1 = covid_data.loc[covid_data["location"] != "World", ["date", "location", "new_cases", "new_deaths"]]
+new_data = new_data1.loc[new_data1["date"] == "2020-03-31", ["location", "new_cases", "new_deaths"]]
+new_cases_mean = np.mean(new_data["new_cases"])
+new_deaths_mean = np.mean(new_data["new_deaths"])
+print(f"This mean has already exclude the rows called 'world'. The mean number for new cases "
+      f"on 2020-3-31 is {new_cases_mean},and the mean number for new deaths "
+      f"on 2020-3-31 is {new_deaths_mean}")
+
+# boxplot of new cases and new deaths on 31 March 2020. The rows "World" are excluded.
 plt.boxplot(x=new_data["new_deaths"])
 plt.title("new deaths on 2020-03-31")
 plt.show()
@@ -50,40 +38,37 @@ plt.boxplot(x=new_data["new_cases"])
 plt.title("new cases on 2020-03-31")
 plt.show()
 
+data = pd.DataFrame({'new_cases': new_data["new_cases"], 'new_deaths': new_data["new_deaths"]})
+data.boxplot(column=['new_cases', 'new_deaths'])
+plt.title("new cases and deaths on 2020-03-31")
+plt.show()
+
+# plot both new cases and new deaths worldwide over time.
 world_data = covid_data.loc[covid_data["location"] == "World", "new_cases"]
 world_data1 = covid_data.loc[covid_data["location"] == "World", "new_deaths"]
 world_dates = covid_data.loc[covid_data["location"] == "World", "date"]
 
-plt.plot(figsize=(30,5))
-# subplots means I can manage different map layers through "axes". figsize controls the length and width.
-
+plt.plot(figsize=(30, 5))
 plt.plot(world_dates, world_data, 'b--', label='new cases')
 plt.plot(world_dates, world_data1, 'r--', label='new deaths')
-
-'''handles1, labels1 = ax1.get_legend_handles_labels()
-handles2, labels2 = ax2.get_legend_handles_labels()
-plt.legend(handles=handles1 + handles2, labels=labels1 + labels2)
-# legends'''
 plt.xticks(world_dates.iloc[0:len(world_dates):4])
 # show x ticks every 4 days.
-plt.xticks(rotation=90,fontsize=6)
+plt.xticks(rotation=90, fontsize=6)
 # rotate the ticks
 plt.ylabel('Number')
 plt.title("worldwide COVID-19 new deaths and new cases overtime")
 plt.legend()
 plt.show()
 
-
-
-china_data=covid_data.loc[covid_data['location']=='China',['total_cases','total_deaths']]
-
-china_rate=china_data.apply(lambda china_data:china_data['total_deaths']/china_data['total_cases'],axis=1)
-# let the value of total death / total cases in each row.
-print(china_rate)
-fig,ax=plt.subplots()
-ax.plot(world_dates,china_rate,"bo")
+# question
+china_data = covid_data.loc[covid_data['location'] == 'China', ['total_cases', 'total_deaths']]
+china_rate = china_data.apply(lambda china_data: china_data['total_deaths'] / china_data['total_cases'], axis=1)
+# let the value of total death / total cases write into each row.
+plt.plot(world_dates, china_rate, 'bo')
 plt.xticks(world_dates.iloc[0:len(world_dates):4])
+plt.xticks(rotation=90, fontsize=6)
 plt.title("China COVID-19 death rate overtime")
-ax.set_xticklabels(ax.get_xticklabels(),rotation=90,fontsize=6)
+plt.ylabel('Total death rate')
+
 plt.show()
 # Opposite to my hypothesis, there is no obvious change on COVID-19 death rate in China.
